39f66c837d99f4435bbe1d21fd8aa6033a476127
GZYZG <2227423992@qq.com>
2021-04-14 23:11:46 +0800

    对分割后的器官进行可视化，并以不同的颜色和透明度进行渲染，还未完成。 主要涉及的函数：vtkColorTransferFunction(), vtkPiecewiseFunction

--- analysis/medical_img.py
+++ analysis/medical_img.py
@@ -2,7 +2,47 @@ import os
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
 import vtk
+from config import config
 
+
+class MyEvent(vtk.vtkInteractorStyleTrackballCamera):
+    def __init__(self, parent=None):
+        self.AddObserver("MiddleButtonPressEvent", self.middle_button_press_event)
+        self.AddObserver("MiddleButtonReleaseEvent", self.middle_button_release_event)
+        self.AddObserver("LeftButtonPressEvent", self.left_button_press_event)
+        self.AddObserver("LeftButtonReleaseEvent", self.left_button_release_event)
+        self.AddObserver("RightButtonPressEvent", self.right_button_press_event)
+        self.AddObserver("RightButtonReleaseEvent", self.right_button_release_event)
+
+    def middle_button_press_event(self, obj, event):
+        print("Middle Button pressed")
+        self.OnMiddleButtonDown()
+        return
+
+    def middle_button_release_event(self, obj, event):
+        print("Middle Button released")
+        self.OnMiddleButtonUp()
+        return
+
+    def left_button_press_event(self, obj, event):
+        print("Left Button pressed")
+        self.OnLeftButtonDown()
+        return
+
+    def left_button_release_event(self, obj, event):
+        print("Left Button released")
+        self.OnLeftButtonUp()
+        return
+
+    def right_button_press_event(self, obj, event):
+        print("right Button pressed")
+        self.OnRightButtonDown()
+        return
+
+    def right_button_release_event(self, obj, event):
+        print("right Button released")
+        self.OnLeftButtonUp()
+        return
 
 def nii2dcm_2(nii_path: str, dcm_path: str):
     image = sitk.ImageSeriesReader_GetGDCMSeriesIDs().ReadImage("01.dcm")  # 读取一个含有头信息的dicom格式的医学图像
@@ -46,16 +86,20 @@ def viz_NII_3D_VR(nii_path):
     property = vtk.vtkVolumeProperty()
 
     popacity = vtk.vtkPiecewiseFunction()
-    popacity.AddPoint(1000, 0.0)
-    popacity.AddPoint(4000, 0.68)
-    popacity.AddPoint(7000, 0.83)
+    for i in range(0, 14):
+        popacity.AddPoint(i, i / 14)
+        # popacity.AddPoint(1000, 0.0)
+        # popacity.AddPoint(4000, 0.68)
+        # popacity.AddPoint(7000, 0.83)
 
     color = vtk.vtkColorTransferFunction()
-    color.AddHSVPoint(1000, 0.042, 0.73, 0.55)
-    color.AddHSVPoint(2500, 0.042, 0.73, 0.55, 0.5, 0.92)
-    color.AddHSVPoint(4000, 0.088, 0.67, 0.88)
-    color.AddHSVPoint(5500, 0.088, 0.67, 0.88, 0.33, 0.45)
-    color.AddHSVPoint(7000, 0.95, 0.063, 1.0)
+    color.AddRGBPoint(0, 255, 255, 255)
+    for i in range(1, 14):
+        color.AddRGBPoint(i, i/13, i/13, i/13)
+        # color.AddRGBPoint(2, 0.042, 0.73, 0.55)
+        # color.AddRGBPoint(3, 0.088, 0.67, 0.88)
+        # color.AddRGBPoint(4, 0.088, 0.67, 0.88, 0.33, 0.45)
+        # color.AddRGBPoint(5, 0.95, 0.063, 1.0)
 
     property.SetColor(color)
     property.SetScalarOpacity(popacity)
@@ -87,42 +131,76 @@ def viz_NII_3D_VR(nii_path):
     iren.Start()
 
 
+def create_color_table():
+    # 创建一个颜色映射表，若是不自己定义，系统会使用默认的
+    pColorTable = vtk.vtkLookupTable()
+    color_num = 13
+    pColorTable.SetNumberOfColors(color_num)
+    for c in range(color_num):
+        r, g, b, a = [0, 1, .5, .5]
+        pColorTable.SetTableValue(c, r, g, b, a)
+    return pColorTable
+
+
 def viz_NII_3D_SR(nii_path):
     reader = vtk.vtkNIFTIImageReader()
     reader.SetFileName(nii_path)
     reader.Update()
 
-    surface = vtk.vtkContourFilter()
+    surface = vtk.vtkMarchingCubes()  # 以Marching Cubes形式进行面绘制
+    # surface = vtk.vtkContourFilter()  # 以等值面的形式进行面绘制
     surface.SetInputConnection(reader.GetOutputPort())
     for i in range(13):
         surface.SetValue(i, i)
 
+    # scalars = vtk.vtkFloatArray()
+    # dims = reader.GetOutput().GetDimensions()
+    # size = dims[0] * dims[1] * dims[2]
+    # for i in range(size):
+    #     scalars.InsertTuple1(i, i % 13)
+    #
+    # reader.GetOutput().GetPointData().SetScalars(scalars)
+
     surface_normals = vtk.vtkPolyDataNormals()
     surface_normals.SetInputConnection(surface.GetOutputPort())
-    surface_normals.SetFeatureAngle(60)
+    surface_normals.SetFeatureAngle(90)
 
     surface_mapper = vtk.vtkPolyDataMapper()
     surface_mapper.SetInputConnection(surface_normals.GetOutputPort())
     surface_mapper.ScalarVisibilityOff()
-
+    clr_table = create_color_table()
+    clr_table.Build()
+    surface_mapper.SetScalarRange(0, 14)
+    surface_mapper.SetLookupTable(clr_table)
     actor = vtk.vtkActor()
     actor.SetMapper(surface_mapper)
 
+    # color = vtk.vtkColorTransferFunction()
+    # color.AddRGBPoint(0, 255, 255, 255)
+    # for i in range(1, 14):
+    #     color.AddRGBPoint(i, i / 13, i / 13, i / 13)
+    # property = vtk.vtkProperty()
+    # property.SetColor(color)
+    actor.GetProperty().SetColor(1, 0, 0)  # (property)
+
     ren = vtk.vtkRenderer()
     ren.AddActor(actor)
-    ren.SetBackground(.1, .2, .4)
+    # ren.SetBackground(.1, .2, .4)
 
     renWin = vtk.vtkRenderWindow()
     renWin.AddRenderer(ren)
 
     iren = vtk.vtkRenderWindowInteractor()
     iren.SetRenderWindow(renWin)
+    iren.SetInteractorStyle(MyEvent())
+    iren.Initialize()
     renWin.SetSize(600, 600)
+
     renWin.Render()
     iren.Start()
-    pass
 
 
 if __name__ == "__main__":
     # nii2dcm("../dataset/1.nii.gz", "../dataset/4/")
-    viz_NII_3D_SR("../dataset/val/pred/organ0004.nii.gz")
+    nii_path = os.path.join(config.val_dataset_dir, "GT/label0004.nii.gz")
+    viz_NII_3D_VR(nii_path)
